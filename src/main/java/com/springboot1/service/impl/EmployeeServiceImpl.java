c63a046acba37c366bc594be15f55185c8a02929
hakim12345 <hakk.ansari92@gmail.com>
2018-06-01 18:54:15 +0530

    getbyid and create method is inser...

--- src/main/java/com/springboot1/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/springboot1/service/impl/EmployeeServiceImpl.java
@@ -10,7 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 @Service
-@Transactional(readOnly = false)
+@Transactional
 public class EmployeeServiceImpl implements EmployeeService {
 
     @Autowired
